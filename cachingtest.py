f4f988d140866d7c075075bf1e7602fd68b99d80
unknown <nahum@rdlabs.org>
2015-04-30 21:49:55 +0200

    adding python

---
+++ cachingtest.py
@@ -0,0 +1,16 @@
+from SocketServer import ThreadingMixIn
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+import datetime
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(datetime.datetime.now())
+
+class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
+    pass
+
+server = ThreadingHTTPServer(("",port), Handler)
+server.serve_forever()
+
